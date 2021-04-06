c5084ed7aa2a8fb301bb11317a4cecf9237a218b
AnwarHossainSR <mahedianwar@gmail.com>
2021-04-06 08:20:53 +0600

    Project setup with api

---
+++ src/component/Cards/Cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card,CardContent,Typography,Grid } from '@material-ui/core'
+
+const Cards = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Cards</h1>
+        </div>
+    )
+}
+
+export default Cards
